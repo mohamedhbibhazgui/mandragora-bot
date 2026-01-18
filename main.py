57f438e50ad2dc14e5bf9142f88881a2d6346d84
Mohamed Habib <mohamedhbibhazgui@gmail.com>
2026-01-18 18:01:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ GOON_MESSAGES = [
     "you got it, boss",
     "yeah yeah, makes sense baws",
 ]
-
+last_random_send = None
 # ─────────────────────────────
 # Allowed color roles
 # ─────────────────────────────
@@ -180,5 +180,27 @@ async def on_message(message):
 
     if contains_goon(message.content):
         await message.channel.send(random.choice(GOON_MESSAGES))
+    user_message = message.content.lower()
 
+    #fuckass command requested by newspaper
+    if user_message == 'victorian cuisine':
+        await message.channel.send(
+            "https://images-ext-1.discordapp.net/external/cgUQPEYpzmj7jm5D1R1lwVw_OHlHeaVU4XdY1W8E8T8/https/i.imgur.com/exNU6Rf.mp4"
+        )
+
+    #random roll (1 / 999 chance) to send some bullshit
+    if random.randint(1, 999) == 2:
+        now = datetime.datetime.utcnow()
+        # check weekly cooldown
+        if (
+            last_random_send is None
+            or now - last_random_send >= datetime.timedelta(days=7)
+        ):
+            await message.channel.send("BORN TO CAST VICTORIA IS A FUCK 鬼神 Kill Em All 1091 I am rock cat410,757,864,530 DEAD VICTORIANS")
+            last_random_send = now
+    #another command requested by newspaper
+    if user_message == "hatto":
+        await message.channel.send(
+            "https://media.discordapp.net/attachments/1432125742396735532/1453363990511091762/hatto.jpg"
+        )
 client.run(TOKEN)
